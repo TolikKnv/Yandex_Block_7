1708f48a75bd3a3c20b3a5ca64fd5204184e98bc
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-02-09 00:46:15 +0300

    08.02.2026

---
+++ lesson 1/Task_5.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
+
+# Напишите SQL запрос в строке.
+results = cur.execute('''
+SELECT category, AVG(price)
+FROM ice_cream
+GROUP BY category;
+''')
+
+
+for result in results:
+    print(result)
+
+con.close()
